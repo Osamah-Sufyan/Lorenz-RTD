d355829f433f854acee4ea7873815a230843f3d5
Osamah-Sufyan <81887592+Osamah-Sufyan@users.noreply.github.com>
2024-08-26 19:04:08 +0200

    Update lorentz_rtd.py

--- lorentz_rtd.py
+++ lorentz_rtd.py
@@ -6,10 +6,13 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+
 import json
 
 
-
+t_end = 1000e-9
 
 
 
@@ -44,7 +47,7 @@ class DifferentialEquationSolver:
     #     else:
     #         return 750e-3 # no pulse outside the interval
     def Vm(self, V0, t):
-        dt = 6000e-9 / 6000000
+        dt = t_end / 22500000
         index = int(t / dt)
         if index < 0:
             index = 0
@@ -92,7 +95,7 @@ class DifferentialEquationSolver:
         for _ in range(num_steps):
             results.append(y)
             I_values.append(y[1]) 
-            V_values.append(y[0]) 
+            V_values.append(y[0])  
             y = self.rk4_step(y, t, dt)
             t += dt
         return np.array(results), np.array(I_values), np.array(V_values)
@@ -129,6 +132,8 @@ with open('lorentz_system_data.json', 'r') as jsonfile:
 # plt.savefig('lorentz_attractor.pdf')
 # plt.show()
 
+# x=x[:round(len(x)/400)]
+# t=t[:round(len(t)/400)]
 print (len(x))
 
 buffer_data_1_t = t[:round(len(t)/8)]
@@ -143,7 +148,7 @@ test_data_x = x[round(5*len(t)/8):]
 print(f"total divisions: ",len(buffer_data_1_t)+len(train_data_t)+len(buffer_data_2_t)+len(test_data_t))
 x_min = np.min(x)-0.1
 x_max = np.max(x)+0.1
-no_virtual_nodes = 30
+no_virtual_nodes = 15
 random_heights = np.random.uniform(0, 1, no_virtual_nodes)
 int_len = t[1]-t[0]
 mask = np.linspace(t[0]-int_len, t[0]+ int_len, 1500)  
@@ -185,7 +190,11 @@ for i in range(0, len(h_input_flattened)):
 
 
 print(f"number step indices: ",len(step_indices))
-V0 = 750e-3 + 1e-3*h_input_flattened
+
+#normalize h_input_flattened
+h_input_flattened = (h_input_flattened - np.min(h_input_flattened)) / (np.max(h_input_flattened) - np.min(h_input_flattened))
+V0 = 750e-3 + 1e-2*h_input_flattened
+
 
 
 
@@ -200,11 +209,11 @@ params = {
 solver = DifferentialEquationSolver(params)
 initial_conditions = [750e-3, 75e-6]
 
-time_step = 6000e-9  / len(h_input_flattened)
+time_step = t_end  / len(h_input_flattened)
 print(f"time step: ", time_step)
 
 
-results, I_values, V_values = solver.solve(initial_conditions, 0e-10, 6000e-9, time_step )
+results, I_values, V_values = solver.solve(initial_conditions, 0e-10, t_end, time_step )
 
 
 data = {'t': t.tolist(), 'I': I_values.tolist()}
@@ -220,19 +229,105 @@ print(I_values)
 y_min = min(I_values)
 y_max = max(I_values)
 
-fig, axs = plt.subplots(2, 1, figsize=(12, 12))
+# fig = plt.figure(figsize=(20, 4))
 
-axs[0].plot(x_repeated, h_input_flattened, label='masked input')
-axs[0].scatter(t, y, color='red', label='mackey glass')
+# plt.plot(x_repeated, h_input_flattened, label='masked input')
+# plt.scatter(t, x, color='red', label='Lorenz data')
+# plt.xlabel(r'$t_{l}$')
+# plt.legend(loc='upper right')
+#plt.savefig('input_picture.pdf')
+#plt.show()
+
+
+# fig = plt.figure(figsize=(20, 4))
+
+# plt.plot(I_values, label='I values')
+# plt.scatter(step_indices, [I_values[i] for i in step_indices], color='green', label='virtual nodes')
+# plt.xlabel(r't$[ns]$', fontsize=15)
+# plt.ylabel(r'I$[mA]$', fontsize=15)
+# plt.xticks([0, 1e-9/time_step, 2e-9/time_step, 3e-9/time_step, 4e-9/time_step, 5e-9/time_step, 6e-9/time_step, 7e-9/time_step, 8e-9/time_step, 9e-9/time_step, 10e-9/time_step], ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
+# plt.grid(True)
+# plt.legend(loc='upper right')
+#plt.savefig('output_picture.pdf')
+#plt.show()
+
+
+
+# fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(20, 8))
+# fig.tight_layout(pad=3.0)
+
+# # Initialize empty line objects
+# line1, = ax1.plot([], [], label='masked input')
+# scatter1 = ax1.scatter([], [], color='red', label='Lorenz data')
+# line2, = ax2.plot([], [], label='I values')
+# scatter2 = ax2.scatter([], [], color='green', label='virtual nodes')
+
+# # Set up the axes
+# ax1.set_xlabel(r'$t_{l}$')
+# ax1.legend(loc='upper right')
+
+# ax2.set_xlabel(r't$[ns]$', fontsize=15)
+# ax2.set_ylabel(r'I$[mA]$', fontsize=15)
+# ax2.set_xticks([0, 1e-9/time_step, 2e-9/time_step, 3e-9/time_step, 4e-9/time_step, 5e-9/time_step, 
+#                 6e-9/time_step, 7e-9/time_step, 8e-9/time_step, 9e-9/time_step, 10e-9/time_step])
+# ax2.set_xticklabels(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
+# ax2.grid(True)
+# ax2.legend(loc='upper right')
+
+# def init():
+#     line1.set_data([], [])
+#     scatter1.set_offsets(np.empty((0, 2)))
+#     line2.set_data([], [])
+#     scatter2.set_offsets(np.empty((0, 2)))
+#     return line1, scatter1, line2, scatter2
+
+# def animate(frame):
+#     # Update first plot
+#     line1.set_data(x_repeated[:frame], h_input_flattened[:frame])
+#     scatter1.set_offsets(np.column_stack((t[:frame], x[:frame])))
+#     ax1.relim()
+#     ax1.autoscale_view()
+
+#     # Update second plot
+#     line2.set_data(range(frame), I_values[:frame])
+#     current_step_indices = [idx for idx in step_indices if idx < frame]
+#     current_I_values = [I_values[idx] for idx in current_step_indices]
+#     scatter2.set_offsets(np.column_stack((current_step_indices, current_I_values)))
+
+#     ax2.relim()
+#     ax2.autoscale_view()
+
+#     return line1, scatter1, line2, scatter2
+
+# # Create the animation
+# anim = FuncAnimation(fig, animate, frames= int(len(I_values)/5), init_func=init, blit=True, interval=0.1)
+
+# # Save the animation
+# anim.save('dynamic_plots.mp4', writer='ffmpeg', fps=2000)
+
+
+# plt.close(fig)
+
+fig, axs = plt.subplots(2, 1, figsize=(12, 12))
+x_repeated_tenth = x_repeated[:len(x_repeated)//4]
+h_input_flattened_tenth = h_input_flattened[:len(h_input_flattened)//4]
+axs[0].plot(x_repeated_tenth, h_input_flattened_tenth, label='masked input')
+
+#axs[0].scatter(t[:len(t)//4], x[:len(x)//4], color='red', label='Lorenz')
+
 axs[0].set_xlabel(r'$t_{mg}$')
 axs[0].legend(loc='upper right')
-
-axs[1].plot(I_values)
-axs[1].scatter(step_indices, [I_values[i] for i in step_indices], color='red', label='Step Indices')
+I_values_nodes = [I_values[i] for i in step_indices]
+ 
+# I_values_tenth = I_values_nodes[:len(I_values)//10]   
+# step_indices_tenth = step_indices[:len(step_indices)//10]
+# I_values_nodes_tenth = [I_values[i] for i in step_indices_tenth]    
+axs[1].plot(I_values[:len(I_values)//4], label='I values')
+axs[1].scatter(step_indices[:len(step_indices)//4], [I_values[i] for i in step_indices[:len(step_indices)//4]], color='red', label='Step Indices')
 axs[1].set_xlabel(r't$[ns]$', fontsize=15)
 axs[1].set_ylabel(r'I$[mA]$', fontsize=15)
-# axs[1].set_xticks([0, 10e-9/time_step, 20e-9/time_step, 30e-9/time_step, 40e-9/time_step, 50e-9/time_step, 60e-9/time_step, 70e-9/time_step, 80e-9/time_step, 90e-9/time_step, 100e-9/time_step])
-# axs[1].set_xticklabels(['0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1'])
+# axs[1].set_xticks([0, 1e-9/time_step, 2e-9/time_step, 3e-9/time_step, 4e-9/time_step, 5e-9/time_step, 6e-9/time_step, 7e-9/time_step, 8e-9/time_step, 9e-9/time_step, 10e-9/time_step])
+# axs[1].set_xticklabels(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
 # axs[1].tick_params(axis='x', labelsize=14)
 # axs[1].set_yticks(y_ticks)
 # axs[1].set_yticklabels(yticks_labels)
